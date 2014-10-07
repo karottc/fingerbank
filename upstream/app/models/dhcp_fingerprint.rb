b0d37bb0468be3e24e213b4ba4c1629acbc91f1a
Julien Semaan <jul.semaan@gmail.com>
2014-10-07 07:50:02 -0400

    Add missing associations in models

--- upstream/app/models/dhcp_fingerprint.rb
+++ upstream/app/models/dhcp_fingerprint.rb
@@ -1,5 +1,7 @@
 class DhcpFingerprint < ActiveRecord::Base
 
+  has_many :combinations
+
   default_scope { order('value') }
 
   validates_uniqueness_of :value
--- upstream/app/models/dhcp_vendor.rb
+++ upstream/app/models/dhcp_vendor.rb
@@ -1,5 +1,6 @@
 class DhcpVendor < ActiveRecord::Base
 
+  has_many :combinations
 
   validates_uniqueness_of :value
 
